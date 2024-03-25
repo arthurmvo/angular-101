e9c25719771a07f5843c747a99eba7ad45c86491
Arthur Martins <arthurmvo@arthurs-air.lan>
2024-03-24 21:19:47 -0300

    Second commit

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
-  styleUrl: './first-component.component.css'
+  styleUrl: './first-component.component.css',
 })
 export class FirstComponentComponent {
-
+  name: string = 'Arthur';
+  age: number = 24;
+  cars: string[] = ['Nivus', 'Chevrolet', 'Toyota', 'BMW'];
 }
